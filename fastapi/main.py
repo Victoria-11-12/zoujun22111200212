599fc661dc3af452a107aba8dfed20af74864971
Victoria <2162096926@qq.com>
2026-05-07 16:50:31 +0800

    增加显式options

--- fastapi/main.py
+++ fastapi/main.py
@@ -2,8 +2,9 @@ import os
 
 os.environ['DOCKER_HOST'] = 'npipe:////./pipe/docker_engine'
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import Response
 from dotenv import load_dotenv
 
 from app.routers import user, admin, chart, analyst
@@ -16,16 +17,32 @@ app = FastAPI(
     version="1.0.0"
 )
 
+ALLOWED_ORIGINS = [
+    "http://43.111.237.98:3000",
+    "http://localhost:3000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://43.111.237.98:3000",
-        "http://localhost:3000",
-    ],
+    allow_origins=ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.middleware("http")
+async def cors_preflight_handler(request: Request, call_next):
+    origin = request.headers.get("origin")
+    if request.method == "OPTIONS" and origin in ALLOWED_ORIGINS:
+        response = Response()
+        response.headers["Access-Control-Allow-Origin"] = origin
+        response.headers["Access-Control-Allow-Credentials"] = "true"
+        response.headers["Access-Control-Allow-Methods"] = "*"
+        response.headers["Access-Control-Allow-Headers"] = "*"
+        return response
+    response = await call_next(request)
+    return response
 
 app.include_router(user.router, prefix="/api", tags=["用户"])
 app.include_router(admin.router, prefix="/api/admin", tags=["管理员"])
