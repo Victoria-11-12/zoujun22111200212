8fa31772f561b1a5ffa26ba7686963c9e150ba16
Victoria <2162096926@qq.com>
2026-05-07 16:20:28 +0800

    跨域访问1

--- fastapi/main.py
+++ fastapi/main.py
@@ -18,7 +18,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://43.111.237.98:3000",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
